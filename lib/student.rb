3544752ef896101b066bda5f1706fa8b8d12d105
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-03-12 18:42:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,5 +5,11 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+  attr_accessor :id, :name, :grade
+def students(name, grade, id = nil)
+  @name = name
+  @grade = grade
+  @id = id 
+end 
 
 end
